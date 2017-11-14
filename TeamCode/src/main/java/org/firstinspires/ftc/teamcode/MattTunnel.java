56a1c42267670465e62147b852cd1bf7d4b9f695
palmer <landonjpalmer@gmail.com>
2017-11-14 17:13:12 -0600

    Hardware mapping matt tunnel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattTunnel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MattTunnel.java
@@ -11,23 +11,20 @@ public class MattTunnel {
     private DcMotor liftLeft;
     private DcMotor liftRight;
     private DcMotor inTake;
-    private Servo   frontRightTunnel;
-    private Servo   backRightTunnel;
-    private Servo   frontLeftTunnel;
-    private Servo   backLeftTunnel;
+    private Servo frontRightTunnel;
+    private Servo backRightTunnel;
+    private Servo frontLeftTunnel;
+    private Servo backLeftTunnel;
 
 
     public MattTunnel(DcMotor ll, DcMotor rl, DcMotor iT, Servo fRT, Servo bRT, Servo fLT, Servo blT) {
-        liftLeft         = ll;
-        liftRight        = rl;
-        inTake           = iT;
+        liftLeft = ll;
+        liftRight = rl;
+        inTake = iT;
         frontRightTunnel = fRT;
-        backRightTunnel  = bRT;
-        frontLeftTunnel  = fLT;
-        backLeftTunnel   = blT;
-
-        
-
+        backRightTunnel = bRT;
+        frontLeftTunnel = fLT;
+        backLeftTunnel = blT;
 
     }
 
@@ -37,48 +34,52 @@ public class MattTunnel {
          *
          *  spin intake motors
          */
-        //throw new UnsupportedOperationException("Doesn't work yet");
-        if(isKeyPressed) {
+
+        if (isKeyPressed) {
             inTake.setPower(1.0);
-        }
-        else {
+        } else {
             inTake.setPower(0.0);
         }
     }
 
     public void setBlocks(boolean isKeyPressed) {
-        if(isKeyPressed) {
-            frontLeftTunnel.setDirection(-1.0);
-            backLeftTunnel.setDirection(-1.0);
-            frontRightTunnel.setDirection(1.0);
-            backRightTunnel.setDirection(1.0);
+        if (isKeyPressed) {
+            frontLeftTunnel.setPosition(0);
+            backLeftTunnel.setPosition(0);
+            frontRightTunnel.setPosition(1.0);
+            backRightTunnel.setPosition(1.0);
         }
     }
 
     public void releaseBlocks(boolean isKeyPressed) {
-        if(isKeyPressed) {
-            frontLeftTunnel.setDirection(1.0);
-            backLeftTunnel.setDirection(1.0);
-            frontRightTunnel.setDirection(-1.0);
-            backRightTunnel.setDirection(-1.0);
+        if (isKeyPressed) {
+            frontLeftTunnel.setPosition(1.0);
+            backLeftTunnel.setPosition(1.0);
+            frontRightTunnel.setPosition(0);
+            backRightTunnel.setPosition(0);
         }
     }
 
     public void manipulateLift(double joyStick) {
         if (joyStick < 0) {
             lowerLift(joyStick);
-        }
-        else if (joyStick != 0) {
+        } else if (joyStick != 0) {
             raiseLift(joyStick);
         }
     }
 
     public void raiseLift(double joy) {
-            throw new UnsupportedOperationException("Doesn't Work yet");
+        if (joy < 0) {
+            liftLeft.setPower(-joy); // Edit later
+            liftRight.setPower(joy);
+        }
     }
 
     public void lowerLift(double joy) {
-        throw new UnsupportedOperationException("Doesn't Work yet");
-    }
+        if (joy > 0) {
+            liftLeft.setPower(joy); // Edit later
+            liftRight.setPower(-joy);
+        }
 
+    }
 }
